45f9f2daf550940aa46c5397ee3c35d90b874bdc
GyeongHwa <gyeonghwaa_@naver.com>
2023-10-08 11:27:33 +0900

    sec02-02

---
+++ src/sec02/chap02/Main.java
@@ -0,0 +1,17 @@
+package sec02.chap02;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+        //사뿐히 즈려밟고 가시옵소서
+//        System.out.println("먼저 가, 난 틀렸어");
+        System.out.println("헬로 월드");
+        // 한줄 짜리 주석
+        System.out.println("오른쪽을 봐"); //주석이 요기있네
+
+//        System.out.println("여러줄");
+//        System.out.println("주석 ctrl+/");
+
+    }
+}
