7ad58e1182fbf0292191f1434ff27a227bb99316
GyeongHwa <gyeonghwaa_@naver.com>
2023-10-16 16:42:19 +0900

    sec06 chap02 / 패키지

---
+++ src/sec06/chap02/pkg1/Parent.java
@@ -0,0 +1,9 @@
+package sec06.chap02.pkg1;
+
+public class Parent {
+    private int a = 1;
+    //default .. 같은 패키지, 상속은 안됨
+    int b = 2;
+    protected int c = 3;
+    public int d = 4;
+}
---
+++ src/sec06/chap02/pkg3/Cls1.java
@@ -0,0 +1,6 @@
+
+package sec06.chap02.pkg3;
+
+public class Cls1 {
+    //  자유롭게 작성
+}
